cadbf04fd81a7f73eb98c2faca874d5f0b3e4d31
Jonas Campos <jonas.campos.programacion@hotmail.com>
2026-04-30 23:47:00 +0200

    Configurando el vite para que funcione el multipagina

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,12 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         quienesSomos: resolve(__dirname, 'quienes-somos.html'),
         cursos: resolve(__dirname, 'cursos.html'),
+        blog: resolve(__dirname, 'blog.html'), // <-- AÑADIDO
         contacto: resolve(__dirname, 'contacto.html'),
         login: resolve(__dirname, 'login.html'),
         registro: resolve(__dirname, 'registro.html'),
         avisoLegal: resolve(__dirname, 'aviso-legal.html'),
-        notFound: resolve(__dirname, '404.html'), // <-- AÑADIDO
+        notFound: resolve(__dirname, '404.html'),
         // Subcarpetas
         algoritmos: resolve(__dirname, 'blogs/algoritmos.html'),
         diagramas: resolve(__dirname, 'blogs/diagramas.html'),
